5105df8074b5f97128d79bd06facdca936e8de5a
DustyArmstrong <pseudokitten@gmail.com>
2021-03-05 19:51:49 +0000

    Update disconnect.sh

--- disconnect.sh
+++ disconnect.sh
@@ -6,7 +6,7 @@
 	if ( strrchr (__FILE__ , 'disconnect') ) {
 	$local_connect_value .= ", \n duration : " . round(((getenv('time_duration'))/3600),2) . "  hours, or " . round(((getenv('time_duration'))/60),2) . "  minutes, or " . getenv('time_duration') . "  seconds,\n upload from vpn-client (received) : " . round(((getenv('bytes_received'))/1048576),2) . " MB, \n download to vpn-client (send) : " . round(((getenv('bytes_sent'))/1048576),2) ." MB. \n DISCONNECTED.";
 	}
-  $logfile_update = date('F d h:i:s') . ", " . getenv('common_name') . ", " . getenv('ifconfig_pool_remote_ip') . ", " . getenv('trusted_ip') . ", " . getenv('time_duration') . ", " . getenv('bytes_received') . ", " . getenv('bytes_sent');
+  $logfile_update = date('F d H:i:s') . ", " . getenv('common_name') . ", " . getenv('ifconfig_pool_remote_ip') . ", " . getenv('trusted_ip') . ", " . getenv('time_duration') . ", " . getenv('bytes_received') . ", " . getenv('bytes_sent');
 	notify_all_remote($local_connect_value);
   fwrite($ovpnlog, $logfile_update . "\n");
   fclose($ovpnlog);
